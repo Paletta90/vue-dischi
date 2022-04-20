2ab6bd0b4771f4a5210e756e28ee38d0253a3afd
DavideDiPietro <davide.dipietro3@gmail.com>
2022-04-20 16:35:18 +0200

    Bonus

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,7 @@
 
     <main class="overflow-auto">
 
-      <div>
-
         <MainComp />
-
-      </div>
 
     </main>
 
@@ -61,26 +57,6 @@
     main {
       height: 90vh;
       background-color: $bg-main;
-    }
-
-    .loader {
-      border: 16px solid #f3f3f3;
-      border-radius: 50%;
-      border-top: 16px solid green;
-      width: 120px;
-      height: 120px;
-      -webkit-animation: spin 2s linear infinite;
-      animation: spin 2s linear infinite;
-    }
-
-    @keyframes spin {
-      0% {
-        transform: rotate(0deg);
-      }
-
-      100% {
-        transform: rotate(360deg);
-      }
     }
 
   }
--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,8 +1,11 @@
 <template>
 
-    <div id="contenitore" class="py-5">
+    <!-- Se l'array non è stato popolato assegno le classi per centrale il loader -->
+    <div :class="arrayDisk.length == 0 ? 'd-flex justify-content-center align align-items-center' : '' "
+        id="contenitore" class="py-5">
 
-        <div class="container">
+        <!-- Se l'array è stato riempito allora mando in stampa -->
+        <div v-if="arrayDisk.length > 0" class="container">
 
             <div class="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5">
 
@@ -14,7 +17,11 @@
 
         </div>
 
+        <!-- Loader di caricamento -->
+        <div v-else class="loader"></div>
+
     </div>
+
 
 </template>
 
@@ -35,18 +42,28 @@
             return {
                 // Array dove metto i dati API di tutti i dischi
                 arrayDisk: [],
-                isLoaded: false,
             }
         },
 
+        // methods: {
+        //     callAxios: function () {
+        //         axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+        //             .then((res) => {
+        //                 this.arrayDisk = res.data.response
+        //                 console.log(this.arrayDisk.length)
+        //             })
+        //     }
+        // },
+
         created() {
             axios.get("https://flynn.boolean.careers/exercises/api/array/music")
-                .then((res) => {
-                    this.arrayDisk = res.data.response
-                })
+                    .then((res) => {
+                        this.arrayDisk = res.data.response
+                        console.log(this.arrayDisk.length)
+            })
+
+            // setTimeout(this.callAxios, 3000)
         },
-
-
 
     }
 </script>
@@ -56,5 +73,26 @@
 
     #contenitore {
         @include center-x(70%);
+        height: 100%;
+    }
+
+    .loader {
+        border: 16px solid #f3f3f3;
+        border-radius: 50%;
+        border-top: 16px solid green;
+        width: 120px;
+        height: 120px;
+        -webkit-animation: spin 2s linear infinite;
+        animation: spin 2s linear infinite;
+    }
+
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg);
+        }
+
+        100% {
+            transform: rotate(360deg);
+        }
     }
 </style>
