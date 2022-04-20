a1a527e3b47dcac28592edadf7317920c93c214d
DavideDiPietro <davide.dipietro3@gmail.com>
2022-04-20 14:07:15 +0200

    Header

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
 
   <div id="app">
 
+    <header>
+      <HeaderComp />
+    </header>
+
+    <main>
+
+    </main>
+
   </div>
 
 </template>
@@ -10,21 +18,42 @@
 
   import "bootstrap"
 
+  // Import components
+  import HeaderComp from "./components/HeaderComp.vue";
+
   export default {
     name: 'App',
-    components: {}
+    components: {
+      HeaderComp
+    }
   }
-  
+
 </script>
 
 <style lang="scss">
 
+  @import "./variabili.scss";
+  @import "./mixin.scss";
+
+  // Bootstrap 5
   @import "bootstrap/dist/css/bootstrap.min.css";
 
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+
+    header{
+      width: 100%;
+      height: 10vh;
+      background-color: $bg-header;
+      @include flex(row, initial, center)
+    }
+
+    main{
+      height: 90vh;
+      background-color: $bg-main;
+    }
   }
 
 </style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <div>
+        <img src="../assets/Loghi/spotifyLogo.png" alt="" width="50px">
+    </div>
+    
+
+</template>
+
+<script>
+    export default {
+        name: 'headerComp'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
