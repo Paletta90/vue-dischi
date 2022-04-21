5487ba740017f3f3dd64cf1bf47beada5162cc78
DavideDiPietro <davide.dipietro3@gmail.com>
2022-04-21 15:09:38 +0200

    Prendo il valore della select

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -3,12 +3,14 @@
     <div class="d-flex justify-content-between align-items-center h-100 px-3">
         <img src="../assets/Loghi/spotifyLogo.png" alt="" width="50px">
 
-        <select class="form-select form-select-sm ms-auto w-25" aria-label=".form-select-lg example">
-            <option selected>Open this select menu</option>
+        <select class="form-select form-select-sm ms-auto w-25" aria-label=".form-select-lg example" v-model="value">
+            <option :value="null" selected>Choose the genre</option>
+            <option v-for="(item, index) in genere" :key="index" :value="index">{{item}}</option>
+            <!-- <option selected>Open this select menu</option>
             <option value="1">Rock</option>
             <option value="2">Pop</option>
             <option value="3">Jazz</option>
-            <option value="4">Metal</option>
+            <option value="4">Metal</option> -->
         </select>
     </div>
 
@@ -21,7 +23,8 @@
 
         data(){
             return{
-               genere: ''
+               genere: ['Rock', 'Pop', 'Jazz', 'Metal'],
+               value: null
             }
         },
 
