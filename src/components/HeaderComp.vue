04889be6b59b9467098dbe379bdec441967386f2
DavideDiPietro <davide.dipietro3@gmail.com>
2022-04-21 15:58:10 +0200

    Cambio @click con @change

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -4,13 +4,15 @@
         <img src="../assets/Loghi/spotifyLogo.png" alt="" width="50px">
 
         <select class="form-select form-select-sm ms-auto w-25" aria-label=".form-select-lg example" v-model="genre">
+
             <option :value="null" selected>Choose the genre</option>
-            <option v-for="(item, index) in genere" :key="index" :value="item" @click=" $emit( 'functionSearch', genre )">{{item}}</option>
-            <!-- <option selected>Open this select menu</option>
-            <option value="1">Rock</option>
-            <option value="2">Pop</option>
-            <option value="3">Jazz</option>
-            <option value="4">Metal</option> -->
+            <option v-for="(item, index) in genere" :key="index" 
+                :value="item" 
+                @change=" $emit( 'functionSearch', genre )"
+                >
+            {{item}}
+            </option>
+
         </select>
     </div>
 
